f76bab907b65648309a8405a07db089b06e15831
Derek van Vliet <derek@getsetgames.com>
2015-05-06 17:43:42 -0400

    added validation on param lists

--- Source/Flurry/Private/FlurryFunctions.cpp
+++ Source/Flurry/Private/FlurryFunctions.cpp
@@ -20,6 +20,28 @@ NSArray* GetNSStringArray(TArray<FString> FStringArray)
 }
 #endif
 
+bool ValidateParams(FString Function, FString Event, TArray<FString> ParamKeys, TArray<FString> ParamValues)
+{
+	if (ParamKeys.Num() != ParamValues.Num())
+	{
+		UE_LOG(LogFlurry,Error,TEXT("%s failed for event %s because number of param keys (%i) differs from number of param values (%i)"),*Function,*Event,ParamKeys.Num(),ParamValues.Num());
+		return false;
+	}
+	
+	if (ParamKeys.Num() > 10)
+	{
+		UE_LOG(LogFlurry,Error,TEXT("%s failed for event %s because the number of keys (%i) can not be greater than 10"),*Function,*Event,ParamKeys.Num());
+		return false;
+	}
+	
+	if (ParamValues.Num() > 10)
+	{
+		UE_LOG(LogFlurry,Error,TEXT("%s failed for event %s because the number of values (%i) can not be greater than 10"),*Function,*Event,ParamValues.Num());
+		return false;
+	}
+	
+	return true;
+}
 
 void UFlurryFunctions::FlurryStartSession(FString APIKey)
 {
@@ -45,20 +67,22 @@ void UFlurryFunctions::FlurryLogEventWithParam(FString Event, FString ParamKey, 
 
 void UFlurryFunctions::FlurryLogEventWithParams(FString Event, TArray<FString> ParamKeys, TArray<FString> ParamValues, bool Timed)
 {
+	if (ValidateParams("FlurryLogEventWithParams", Event, ParamKeys, ParamValues) == false)
+		return;
+
 #if PLATFORM_IOS
-	NSArray* NSParamKeys = GetNSStringArray(ParamKeys);
-	NSArray* NSParamValues = GetNSStringArray(ParamValues);
-	NSDictionary* Params = [NSDictionary dictionaryWithObjects:NSParamValues forKeys:NSParamKeys];
+	NSDictionary* Params = [NSDictionary dictionaryWithObjects:GetNSStringArray(ParamValues) forKeys:GetNSStringArray(ParamKeys)];
 	[Flurry logEvent:Event.GetNSString() withParameters:Params timed:Timed];
 #endif
 }
 
 void UFlurryFunctions::FlurryEndTimedEvent(FString Event, TArray<FString> ParamKeys, TArray<FString> ParamValues)
 {
+	if (ValidateParams("FlurryEndTimedEvent", Event, ParamKeys, ParamValues) == false)
+		return;
+	
 #if PLATFORM_IOS
-	NSArray* NSParamKeys = GetNSStringArray(ParamKeys);
-	NSArray* NSParamValues = GetNSStringArray(ParamValues);
-	NSDictionary* Params = [NSDictionary dictionaryWithObjects:NSParamValues forKeys:NSParamKeys];
+	NSDictionary* Params = [NSDictionary dictionaryWithObjects:GetNSStringArray(ParamValues) forKeys:GetNSStringArray(ParamKeys)];
 	[Flurry endTimedEvent:Event.GetNSString() withParameters:Params];
 #endif
 }
